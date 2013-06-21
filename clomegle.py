49b41386c26b7ea9acb8efe2ee8576b3608bb640
Ian Whitlock <ian@theknown.net>
2013-06-21 03:08:09 -0400

    clomegle

---
+++ clomegle.py
@@ -0,0 +1,31 @@
+import cleverbot
+import irc.bot
+from time import sleep
+from random import random, randrange
+
+class CleverBot(irc.bot.SingleServerIRCBot):
+    def __init__(self):
+        irc.bot.SingleServerIRCBot.__init__(self, [("irc.freenode.net", 6667)], "Clomegle", "Clomegle")
+        self.bot = cleverbot.Session()
+        self.omeglenick = "snide-sup"
+        self.channel = "##valix-offtopic"
+
+
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+
+    def on_pubmsg(self, c, e):
+        arg = e.arguments[0].encode("ascii", "ignore")
+        if (e.source.split("!")[0] == self.omeglenick):
+            if (arg != "<stranger connected>" and arg != "<stranger disconnected>"):
+                if (arg == "myyy caaaammm goot so daark on heeere buut looooks ok at my blooogg"):
+                    c.privmsg(self.omeglenick, "disconnect")
+                else:
+                    reply = self.bot.Ask(arg)
+		    if (len(reply) > 35):
+			sleep(1 * ((len(reply) / 35) - 1))
+                    c.privmsg(self.channel, self.omeglenick + ": " + reply)
+
+
+bot = CleverBot()
+bot.start()
